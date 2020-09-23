6c0a17dd686f47d31ab2e0604d52f5a5c6b22634
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2020-09-22 21:52:42 -0500

    added current-weather component

---
+++ main.js
@@ -0,0 +1,30 @@
+const { create } = require('domain');
+const {app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({ width: 800, height: 600 });
+
+  // win.loadURL(`file://${__dirname}/dist/local-weather-app/index.html`);
+
+  win.loadURL(`http://localhost:8000`);
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
